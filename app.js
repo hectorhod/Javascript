d4f532cb6353d50d97c5243377500728fd809ab9
Hectorhod <hectorfreitas@hotmail.com>
2021-09-14 11:31:09 -0300

    Add branch com sistema funcionando e css montado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({secret:"3l3f@ntEpr3t0", resave: true, saveUninitialized: true}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
+const modelUser = require('../models/users');
 
 /*
 router.get('/', async (req, res, next) => {
@@ -11,10 +13,31 @@ router.get('/', async (req, res, next) => {
   }
 })*/
 
+/*POST login page*/
+router.post('/', async (req,res)=>{
+  console.log(req.body.email);
+  console.log(req.body.senha);
+  const email = req.body.email;
+
+  if (db.Mongoose.model('users', modelUser.UserSchema).find({email})) {
+    console.log("Usuario Econtrado!");
+    const Login = await db.Mongoose.model('users', modelUser.UserSchema).find({email}).lean().exec();
+    console.log(Login);
+  }else{
+    console.log("Erro");
+  }
+
+  res.render('login', { title: 'Login'});
+});
+
+/*GET login page*/
+router.get('/', (req,res)=>{
+  res.render('login', { title: 'Login'});
+});
 
 /* GET home page. */
 router.get('/', async (req, res) => {
-  const Users = global.db.Mongoose.model('users', db.UserSchema, 'users');
+  const Users = db.Mongoose.model('users', modelUser.UserSchema, 'users');
  
   const docs = await Users.find({}).lean().exec();
   res.render('index', { title: 'Lista de Clientes', docs });
@@ -25,15 +48,29 @@ router.get('/users', (req, res) => {
   res.render('users', { title: 'Cadastro de Usuário' });
 });
 
-/* POST new user */
-router.post('/users', async (req, res) => {
- 
+// POST new user //
+router.post('/users', async (req, res, next) => {
+ /*
   const username = req.body.username;
   const email = req.body.email;
  
   const Users = global.db.Mongoose.model('users', global.db.UserSchema, 'users');
   const user = new Users({ username, email });
- 
+*/
+  
+  const nome = req.body.nome;
+  const sobrenome = req.body.sobrenome;
+  const email = req.body.email;
+  const idade = req.body.idade;
+  const cidade = req.body.cidade;
+  const uf = req.body.uf;
+  const cep = req.body.cep;
+  const senha = req.body.senha;
+
+  const Users = db.Mongoose.model('users', modelUser.UserSchema, 'users');
+  const user = new Users({nome,sobrenome,email,idade,cidade,uf,cep,senha});
+
+
   try {
     await user.save();
     console.log("Usuario criado com exito");
