9591a682af6d121a3352b705648763f939acfa04
Hectorhod <hectorfreitas@hotmail.com>
2021-09-30 15:04:25 -0300

    Atualizando sistema para rodar na nuvem

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const server = 'mongodb://localhost:27017/'
 const database = 'dadosdb';
-//const uri = "mongodb+srv://dbUser:userDB1234@cluster0.qpj9v.mongodb.net/teste?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+const uri = "mongodb+srv://dbUser:userDB1234@cluster0.qpj9v.mongodb.net/testes?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
 
-mongoose.connect(server + database).then(() => {
+mongoose.connect(uri).then(() => {
         console.log("Succesfully connect to MongoDB.");
     })
         .catch(err => {
