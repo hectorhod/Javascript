7449625048d74b74bc0efaca274d20387881ec9d
Hectorhod <hectorfreitas@hotmail.com>
2021-09-30 16:01:47 -0300

    correçao de erros

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 const server = 'mongodb://localhost:27017/'
 const database = 'dadosdb';
-<<<<<<< HEAD
 const uri = "mongodb+srv://dbUser:userDB1234@cluster0.qpj9v.mongodb.net/testes?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
-=======
-//const uri = "mongodb+srv://dbUser:userDB1234@cluster0.qpj9v.mongodb.net/teste?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
->>>>>>> 1b549a05625d5dc0404eb9ac5feaebf5a2df18a6
 
 mongoose.connect(server + database).then(() => {
         console.log("Succesfully connect to MongoDB.");
@@ -16,15 +12,3 @@ mongoose.connect(server + database).then(() => {
         });
 
 module.exports = {Mongoose:mongoose};
-<<<<<<< HEAD
-=======
-/*
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String
-}, { collection: 'users'}
-);
-
-module.exports = {Mongoose:mongoose, UserSchema:userSchema};
-*/
->>>>>>> 1b549a05625d5dc0404eb9ac5feaebf5a2df18a6
